621a472e90390e1ff99826ca4c7319eb6a33c05b
s10wn <izi.mantolk@gmail.com>
2024-08-26 13:40:53 +0200

    fix: mock

--- src/__mocks__/setup-mocks.ts
+++ src/__mocks__/setup-mocks.ts
@@ -1,7 +1,5 @@
 import { setupZodiacSignApiMocks } from "./zodiac-sign-api.mock";
 
 export const setupMocks = (): void => {
-  if (import.meta.env.DEV || import.meta.env.MODE === "staging") {
-    setupZodiacSignApiMocks();
-  }
+  setupZodiacSignApiMocks();
 };
